1cf81e3fd853f1fa9c6808d9df02d8bc7f9da8bb
dekken <philip.deegan@gmail.com>
2016-05-18 16:16:27 +0100

    Cross platform-ness

--- MessTrek/Tests/YourTests.cpp
+++ MessTrek/Tests/YourTests.cpp
@@ -2,12 +2,15 @@
 
 #include "gtest/gtest.h"
 
+#include "StarTrek/Game.h"
+
 TEST(StarTrekCharacterization, SomeAsYetUnknownScenario) {
 	EXPECT_TRUE(false);
 }
 
 int main(int argc, char** argv)
 {
+	StarTrek::Game game;
 	// run all tests
 	::testing::InitGoogleTest(&argc, argv);
 
